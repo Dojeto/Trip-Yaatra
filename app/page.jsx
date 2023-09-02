214290374407260e399870c3ae282d610bc67557
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-02 14:56:29 +0530

    navbar update

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,17 @@ import Booking from "@components/Booking";
 export default function Home() {
   return (
     <main className="h-screen">
+      <div className="">
+      <Navbar />
+      </div>
+      
       <div
         className="bg-cover bg-center h-3/4"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1776&q=80')`,
         }}
       >
-        <Navbar />
+        
         <div className="container mx-auto pt-40 text-center">
           <h1 className="tracking-wider font-homecontent text-3xl font-bold text-white">
             Explore the huge world
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,33 +4,41 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <nav className="p-2 bg-transparent flex justify-between fixed w-full">
-      <Image src="/assets/logo.png" width={150} height={150} alt="logo" />
-      <div className="flex gap-5 items-center">
-        <button className="bg-transparent text-lime-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
-          Home
-        </button>
-        <button className="bg-transparent text-lime-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
-          Reviews
-        </button>
-        <button className="bg-transparent text-lime-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
-          Pakages
-        </button>
-        <button className="bg-transparent text-lime-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
-          Videos
-        </button>
-        <button className="bg-transparent text-lime-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
-          fights
-        </button>
+  
+
+    <nav className=" p-[10px] flex justify-between items-center w-full bg-gray-100  mx-auto h-[75px]">
+      <div>
+        <Image src="/assets/logo.png" width={150} height={150} alt="logo" />
+      </div>
+      <div classNameName="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+        <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
+            Home
+          </button>
+          
+          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
+            Reviews
+          </button>
+          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
+            Pakages
+          </button>
+          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
+            Videos
+          </button>
+          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
+            fights
+          </button>
+        </ul>
       </div>
       <div className="flex gap-3 items-center">
-        <button className="bg-transparent border border-lime-100 text-lime-100 hover:bg-gray-100 hover:text-gray-700 hover:border-gray-700 px-4 py-2 w-28 h-10">
-          Sign-in
-        </button>
-        <button className="bg-transparent border border-lime-100 text-lime-100 hover:bg-gray-100 hover:text-gray-700 hover:border-gray-700 px-4 py-2 w-32 h-10">
-          Sign-Up
-        </button>
-      </div>
+  <button className="bg-gray-100 border border-gray-500 text-gray-700 hover:bg-gray-500 hover:text-white hover:border-gray-700 px-4 py-2 w-28 h-10 rounded-md">
+    Sign-in
+  </button>
+  <button className="bg-gray-100 border border-gray-500 text-gray-700 hover:bg-gray-500 hover:text-white hover:border-gray-700 px-4 py-2 w-32 h-10 rounded-md">
+    Sign-Up
+  </button>
+</div>
+
     </nav>
   );
 };
