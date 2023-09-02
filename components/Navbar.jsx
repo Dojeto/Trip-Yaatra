3fc6e9e698ef0fd3a0bf0ca8af89bd656a22f989
Mann Patel <mannpatel3118@gmail.com>
2023-09-02 17:07:33 +0530

    proto1

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,41 +4,40 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-  
-
-    <nav className=" p-[10px] flex justify-between items-center w-full bg-gray-100  mx-auto h-[75px]">
-      <div>
-        <Image src="/assets/logo.png" width={150} height={150} alt="logo" />
-      </div>
-      <div classNameName="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
-        <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
-            Home
-          </button>
-          
-          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
-            Reviews
-          </button>
-          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
-            Pakages
-          </button>
-          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
-            Videos
-          </button>
-          <button className="bg-transparent text-black-100 hover:bg-gray-200 hover:text-gray-700 px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
-            fights
-          </button>
-        </ul>
+    <nav className="p-2 bg-transparent flex justify-between fixed w-full bg-white rounded-b-xl z-10 drop-shadow-2xl">
+      <Image
+        src="/assets/logo-only-outline.png"
+        width={60}
+        height={75}
+        alt="logo"
+        className="hover:cursor-pointer transform hover:scale-105 transition-transform duration-300 ease-in-out"
+        // className=" bg-white"
+      />
+      <div className="flex gap-5 items-center text-black">
+        <button className="uppercase bg-transparent hover:bg-gray-200 hover:text-gray-700 hover:rounded-md px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-30 h-10">
+          Home
+        </button>
+        <button className="uppercase bg-transparent hover:bg-gray-200 hover:text-gray-700 hover:rounded-md px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
+          Reviews
+        </button>
+        <button className="uppercase bg-transparent hover:bg-gray-200 hover:text-gray-700 hover:rounded-md px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
+          Packages
+        </button>
+        <button className="uppercase bg-transparent hover:bg-gray-200 hover:text-gray-700 hover:rounded-md px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
+          Videos
+        </button>
+        <button className="uppercase bg-transparent hover:bg-gray-200 hover:text-gray-700 hover:rounded-md px-4 py-2 transform hover:scale-105 transition-transform duration-300 ease-in-out w-32 h-10">
+          fights
+        </button>
       </div>
       <div className="flex gap-3 items-center">
-  <button className="bg-gray-100 border border-gray-500 text-gray-700 hover:bg-gray-500 hover:text-white hover:border-gray-700 px-4 py-2 w-28 h-10 rounded-md">
-    Sign-in
-  </button>
-  <button className="bg-gray-100 border border-gray-500 text-gray-700 hover:bg-gray-500 hover:text-white hover:border-gray-700 px-4 py-2 w-32 h-10 rounded-md">
-    Sign-Up
-  </button>
-</div>
-
+        <button className="uppercase bg-transparent border rounded-md border-black  transform hover:scale-105 transition-transform duration-300 ease-in-out px-4 py-2 w-28 h-10">
+          Sign-in
+        </button>
+        <button className="uppercase bg-blue-700 text-white rounded-md border border-black transform hover:scale-105 transition-transform duration-300 ease-in-out px-4 py-2 w-32 h-10">
+          Register
+        </button>
+      </div>
     </nav>
   );
 };
