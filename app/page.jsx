327b583fb813016464861741aefb3b0fa404adf6
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-03 13:12:59 +0530

    top-feature add

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "@components/Navbar";
 import Booking from "@components/Booking";
+import Feature from "@components/Feature";
 export default function Home() {
   return (
     <main className="h-screen">
@@ -24,118 +25,19 @@ export default function Home() {
         </div>
         <Booking />
       </div>
-      <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Exercitationem, iure aliquam voluptatem omnis obcaecati, perferendis
-        nemo ullam dolorem beatae possimus nostrum dolorum totam, aut modi
-        tempore dignissimos. Alias, labore magnam veniam, suscipit recusandae
-        consequuntur totam est minima aut aliquid aperiam dolorem porro?
-        Placeat, necessitatibus! Iste dolor laudantium ut explicabo earum minus
-        nam accusamus eaque fugiat nihil accusantium quisquam id esse
-        dignissimos maxime, illum reprehenderit ducimus non deserunt. Minima
-        illum molestiae eaque ad? Repellat impedit odio possimus, ipsam ratione
-        obcaecati earum quae! Dolorem officia optio aliquam, perferendis harum
-        in totam expedita repudiandae facilis sunt at inventore eum impedit,
-        quia ut commodi est accusamus ad exercitationem amet laudantium! Odit
-        sed architecto accusantium tenetur tempore facere doloribus velit
-        accusamus in ab, provident molestiae corrupti, aspernatur adipisci
-        cumque numquam quo cupiditate dolorem minima rem iste consequuntur
-        dignissimos vero. Autem doloribus reiciendis sequi cupiditate neque
-        nesciunt nemo. Facere et sed ipsam inventore? Pariatur earum possimus
-        voluptas veniam magnam, alias ducimus voluptates placeat qui, minus
-        nostrum aut rerum quod est modi libero ad repellendus debitis iure
-        mollitia consequuntur atque eveniet numquam. Recusandae quo hic
-        aspernatur id quia, alias accusantium tempora assumenda similique, vero
-        veritatis magni quisquam reiciendis cum debitis consectetur pariatur ut.
-        Quis assumenda aspernatur laboriosam, et laudantium ducimus iusto nobis
-        repellendus quaerat odio ut minus natus aliquam sunt impedit consectetur
-        voluptates dolor at iste exercitationem ex. Facilis, ullam dolores.
-        Laboriosam deleniti unde consequatur ut illum veritatis, optio eum nemo,
-        doloribus neque laborum maiores repellendus cum et quod blanditiis odio
-        accusamus! Amet iure omnis iusto aliquid quas quos beatae modi tenetur
-        voluptate nisi nostrum ipsam ipsa eveniet, eius praesentium voluptatum
-        pariatur quod officia aperiam sunt, maxime nihil labore fuga. Dolores,
-        amet perferendis repudiandae aperiam placeat autem nesciunt non
-        temporibus vero aliquid iusto ea reprehenderit quo aspernatur animi
-        voluptate ratione blanditiis officiis provident praesentium sed. Nam
-        exercitationem illo eligendi temporibus asperiores totam distinctio
-        earum possimus dolores beatae ducimus sed laudantium ullam iure culpa,
-        dolorem esse laborum saepe quae repellat fugit consectetur deleniti!
-        Repellat saepe, est exercitationem temporibus, placeat, dicta quasi
-        earum nostrum minima praesentium aperiam! Officiis commodi porro
-        repellendus, amet accusamus voluptate autem dolore rerum quaerat libero
-        architecto. Pariatur, rem cupiditate. Minima quibusdam molestiae eius
-        saepe nesciunt maiores doloremque aperiam magnam repudiandae iste iure,
-        sint in quod mollitia rerum quas, ipsum nemo hic qui velit sequi. Dolor
-        sequi animi ad corporis obcaecati natus recusandae neque provident velit
-        consequuntur, explicabo nisi nemo earum? Cupiditate, illo cumque
-        asperiores ipsa eius dignissimos odio perspiciatis mollitia aliquid
-        laboriosam deserunt aliquam, vitae perferendis itaque distinctio tenetur
-        saepe! Culpa facilis est vel, unde odio ducimus at repudiandae rerum
-        nostrum blanditiis ipsum nesciunt distinctio voluptas? In, unde dolores
-        molestiae cumque, magni perferendis quasi ab sapiente cupiditate quos
-        consequatur minima iusto molestias ducimus odio beatae! Vero facilis
-        fuga, illum quia sint inventore dignissimos amet laboriosam
-        necessitatibus earum non sapiente cupiditate quae blanditiis molestiae
-        officiis dolore optio deserunt vitae cumque. Quaerat expedita velit
-        ipsam quia, at enim obcaecati sit! Quia, dolorum, iusto eligendi veniam
-        quisquam enim, fuga non nam est accusamus aliquid placeat. Accusamus,
-        incidunt mollitia cupiditate, maxime odit, aperiam voluptate voluptas
-        unde inventore delectus officia. Voluptates beatae ut non aliquid quas,
-        dicta voluptatibus dolores nobis. Impedit nihil eum minus voluptatem.
-        Vero distinctio placeat, minima nisi sint a eos inventore id commodi
-        odit reprehenderit suscipit aliquid iure praesentium veniam repellendus
-        dolor porro. Aperiam optio dolores aliquid voluptatum officia ab dolorum
-        distinctio, possimus nisi repellendus sit, quae autem rem minus atque
-        voluptate quibusdam? Consectetur ut rerum aut, commodi corporis harum
-        aliquid quaerat dolorem. Aperiam exercitationem voluptatibus, aspernatur
-        iure aut tempora hic perspiciatis ratione facere odio cupiditate ipsam,
-        perferendis corrupti illum praesentium ullam cum et reprehenderit
-        eveniet. Quisquam ipsam, accusantium illum quasi quis officia cupiditate
-        nisi facilis reiciendis aut eveniet voluptatibus beatae nulla odit
-        suscipit quae veniam. Pariatur, odio quas eveniet corporis corrupti
-        adipisci illum eius placeat aut reprehenderit? Sint doloremque harum
-        fuga enim suscipit vel placeat vitae nostrum aliquam. Cum iste nostrum
-        quisquam dolor totam dicta illo, perspiciatis delectus eos enim officiis
-        dolores! Alias, voluptas! Voluptates perspiciatis eum cupiditate
-        reiciendis libero maxime repellat qui facere nulla explicabo, nisi
-        laboriosam sit totam itaque facilis aliquam, nostrum repellendus. Nihil
-        voluptate adipisci numquam dolore aliquid provident quia, unde aliquam
-        nam nisi accusantium quo. Deleniti quasi sint similique iste nisi,
-        voluptatum suscipit id, unde magni quos recusandae eligendi dolor beatae
-        blanditiis rerum aperiam soluta obcaecati officiis quam rem? Ipsa iusto
-        quidem nobis. Natus laboriosam esse saepe libero perspiciatis accusamus
-        aut temporibus deleniti sapiente eaque magni explicabo labore incidunt
-        officia, molestiae ipsam voluptatum. Praesentium animi hic iste, omnis
-        deserunt, eveniet molestias, eum cum veritatis cumque expedita
-        repudiandae. Odit laboriosam aliquam animi minima beatae voluptate,
-        magnam iure quae. Vitae aliquid, ullam beatae porro nulla deleniti
-        possimus saepe adipisci amet voluptate ea dolorum sequi quam quibusdam
-        deserunt ducimus. Fuga tempore molestiae voluptatem sint eos repellendus
-        illum unde placeat cum reiciendis non iure ducimus distinctio id esse,
-        praesentium doloribus temporibus vero ipsum excepturi qui nesciunt
-        sapiente consequatur? Voluptate doloremque sit ipsum, quibusdam
-        perspiciatis natus est esse labore incidunt illum. Incidunt eveniet
-        temporibus sapiente voluptatem quo voluptate magni vitae, repudiandae
-        modi, iure ea, reiciendis beatae. Est impedit fugiat autem distinctio
-        rem deleniti exercitationem dolore praesentium. Explicabo asperiores
-        beatae, veritatis nobis possimus ad quo facilis saepe voluptatum
-        pariatur totam numquam itaque? Error quos eius ipsum numquam culpa eum,
-        veniam odio officia nisi modi tempore? Eveniet corrupti doloremque aut
-        quidem consequatur commodi nostrum officia repellendus necessitatibus,
-        perspiciatis vel cum nam! Dolore quia quas sit enim autem quidem, veniam
-        quibusdam! Ab, vel soluta non placeat excepturi suscipit repellat
-        expedita iure voluptate quisquam beatae voluptas adipisci possimus
-        provident consequatur! Necessitatibus doloribus quas tenetur delectus
-        ipsum vel odio laudantium, perspiciatis dolores velit aut et voluptates
-        cupiditate voluptatem illum perferendis esse. Asperiores, sint incidunt?
-        Natus, modi quia, repellat rem facere ipsa quidem consequatur fuga
-        voluptates error harum. Tenetur, ex numquam illo omnis ea, optio commodi
-        rem, animi sapiente veniam excepturi. Architecto neque voluptates,
-        voluptas facere numquam est? Doloribus sequi consequuntur quos! Tempore
-        quaerat repellendus molestiae mollitia inventore. Unde perferendis,
-        sint, iste eveniet odio dicta doloribus eum reprehenderit consequatur
-        fugit quo, nemo veritatis.
+      <div className="bg-gray-200 h-full">
+        <div className="container mx-auto pt-[30px] h-full">
+          <div className="flex items-center justify-center flex-col text-center">
+            <span class="text-4xl font-bold ml-4 transition-transform transform hover:scale-105 text-[#3b3b6e]">
+              Explore the Hottest Destinations
+            </span>
+            <span class="text-xl mt-2 custom-blur">Trending this Month</span>
+          </div>
+
+          <div className="flex justify-center mt-8 mx-2">
+            <Feature />
+          </div>
+        </div>
       </div>
     </main>
   );
---
+++ components/Feature.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+
+const Feature = () => {
+  return (
+    <div className="flex flex-wrap justify-center gap-4">
+      {/* Repeat this card section for each card */}
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="w-[300px]">
+        <div class="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
+          <a href="#" class="block rounded-t-lg">
+            <img src="/assets/bg3.jpg" alt="Article Image" />
+          </a>
+          <div class="p-3 flex flex-col items-center justify-center">
+            <a
+              href="#"
+              class="block mb-2 text-lg font-bold text-gray-900 dark:text-white hover:text-blue-700 dark:hover:text-blue-300 transition-colors text-center">
+              BALI
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* Repeat this card section for each card */}
+    </div>
+  );
+};
+
+export default Feature;
