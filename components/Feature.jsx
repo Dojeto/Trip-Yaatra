e2cf4bfab6d2fcd35972c8f249ba41e50fc09577
Mann Patel <mannpatel3118@gmail.com>
2023-09-06 00:07:33 +0530

    remove unwanted code

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -77,29 +77,3 @@ const Feature = () => {
 };
 
 export default Feature;
-
-// <div className="flex flex-wrap justify-center gap-4">
-//   {/* Repeat this card section for each card */}
-//   <div className="w-[300px]">
-//     <div className="max-w-xs mx-auto bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-xl dark:bg-gray-800 dark:border-gray-700 transition-transform transform hover:-translate-y-1 hover:scale-105">
-//       <Link href="#" className="block rounded-t-lg">
-//         <Image
-//           src="/assets/bg3.jpg"
-//           alt="Article Image"
-//           width={300}
-//           height={300}
-//         />
-//       </Link>
-//       <div className="p-3 flex flex-col items-center justify-center">
-//         <Link
-//           href="#"
-//           className="block mb-2 text-lg font-bold text-gray-900 dark:text-white dark:hover:text-blue-300 transition-colors text-center"
-//         >
-//           BALI
-//         </Link>
-//       </div>
-//     </div>
-//   </div>
-
-//   {/* Repeat this card section for each card */}
-// </div>
