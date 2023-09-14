082a1d1f78d35525b43846001b113b96846096c1
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-09-15 02:08:50 +0530

    social media icon and review section add

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Navbar from "@components/Navbar";
 import Hero from "@components/Hero";
 import Feature from "@components/Feature";
 import Packages from "@components/Packages";
+import Review from "@components/Review";
 export default function Home() {
   return (
     <main className="h-screen">
@@ -12,6 +13,9 @@ export default function Home() {
       </div>
       <div>
         <Packages />
+      </div>
+      <div>
+        <Review />
       </div>
     </main>
   );
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Search from "./Search";
+
+
+import Link from "next/link";
+import { FaFacebookF ,FaTwitter ,FaLinkedinIn} from "react-icons/fa";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+
 const Hero = () => {
   return (
     <div
@@ -16,6 +22,35 @@ const Hero = () => {
           out there and enjoy its beauty
         </p>
       </div>
+      {/* social media */}
+      <div className="absolute top-0 right-0 h-full flex flex-col items-end justify-center space-y-6 p-4 mr-[25px]">
+        {/* section */}
+        <div className="text-center p-2 rounded-full  text-2xl">
+          <Link href="#">
+           <FaFacebookF className="text-white"/>
+          </Link>
+        </div>
+        <div className="text-center p-2 rounded-full  text-2xl">
+          <Link href="#" className="block">
+          <BiLogoInstagramAlt className="text-white"/>
+          </Link>
+        </div>
+        <div className="text-center p-2 rounded-full text-2xl">
+          <Link href="#" className="block">
+            <FaTwitter className="text-white"/>
+          </Link>
+        </div>
+        <div className="text-center p-2 rounded-full text-2xl">
+          <Link href="#" className="block">
+            <FaLinkedinIn className="text-white"/>
+          </Link>
+        </div>
+
+        {/* section */}
+      </div>
+
+
+      
       <div className="w-8 absolute bottom-10 left-1/2">
         <svg
           viewBox="0 0 24 24"
@@ -28,7 +63,7 @@ const Hero = () => {
             fill="currentColor"
           ></path>
         </svg>
-
+       
         <span className="main__scroll-text text-white ">Scroll</span>
       </div>
       <Search />
