4093adb24fe087c52f0a9f84d36441a7ca52aee7
Priyanshu Patel <patelpriysnshu2410@gmail.com>
2023-10-13 17:19:23 +0530

    blogs update

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -4,78 +4,38 @@ import React from 'react'
 const Blogs = () => {
     return (
         <div className="pt-[125px]">
-            <div className="w-[100%] h-[650px] object-cover relative">
-                <img src="/assets/blogs.jpg" alt="" className="w-full h-full" />
-                <h1 className="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold">
-                    Let’s Take a look as to what the coolest travel community says
-                </h1>
-                <h1 className="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold mt-[70px]">
-                    about the upcoming travel trends!
-                </h1>
+            <div className="w-full h-[660px] relative">
+                <img src="/assets/blog1.jpg" alt="" className="w-full h-full object-cover" />
             </div>
 
-            <div className=" mt-[50px]  px-36 flex flex-col justify-center items-center ">
-                <div>
-                    <h2 className=" m-3 font-bold text-center text-darkbl text-4xl">
-                        Our Blogs
-                    </h2>
+            <div className=" mt-[40px]   flex flex-col justify-center items-center ">
+                     
+                <div className=" grid grid-cols-3 gap-x-8 mt-18 gap-y-8">
+
+                    <div className="max-w-[350px] bg-white  border-b-2 border-black  ">
+                        <Link href="#" className='w-fit'>
+                            <img className="rounded-t-lg w-full" src="/assets/blog1.jpg" alt="" />
+                        </Link>
+                        <div className="p-5">
+                            <div >
+                                <h2 className='mb-2 text-[13px] '>Your Place, Oct 24, 2021</h2>
+                            </div>
+                            <div >
+                                <h5 className="mb-2 text-[20px] font-bold tracking-tight text-gray-900 dark:text-black">Noteworthy technology acquisitions 2021</h5>
+                            </div>
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                            <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-darkbl rounded-lg transform hover:scale-105 transition-transform duration-300 ease-in-out  ">
+                                Read more
+                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </Link>
+                        </div>
+                    </div>
                     
-                </div>
-                <div className=" bg-highlight w-[120px] h-[3px] mt-1 mb-11"></div>
-                <div className=" grid grid-cols-3 gap-x-3 mt-18 gap-y-5">
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700">
-                        <Link href="#" className='w-fit'>
-                            <img className="rounded-t-lg w-full" src="/assets/AboutUs/aboutus1.png" alt="" />
-                        </Link>
-                        <div className="p-5">
-                            <Link href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Noteworthy technology acquisitions 2021</h5>
-                            </Link>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                            <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700">
-                        <Link href="#" className='w-fit'>
-                            <img className="rounded-t-lg w-full" src="/assets/AboutUs/aboutus1.png" alt="" />
-                        </Link>
-                        <div className="p-5">
-                            <Link href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Noteworthy technology acquisitions 2021</h5>
-                            </Link>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                            <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700">
-                        <Link href="#" className='w-fit'>
-                            <img className="rounded-t-lg w-full" src="/assets/AboutUs/aboutus1.png" alt="" />
-                        </Link>
-                        <div className="p-5">
-                            <Link href="#">
-                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">Noteworthy technology acquisitions 2021</h5>
-                            </Link>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                            <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </Link>
-                        </div>
-                    </div>
-                   
-                   
+                    
+
+
                 </div>
             </div>
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           }
         >
           <ul className="flex flex-col text-white justify-center items-stretch">
-            <li className=" border-b border-gray-300 py-2 px-6">
+            <div className=" border-b border-gray-300 py-2 px-6">
               <Link
                 href={"/international-trips"}
                 shallow
@@ -38,8 +38,8 @@ const Navbar = () => {
                 {" "}
                 International Packages
               </Link>
-            </li>
-            <li className=" border-b border-gray-300 py-2 px-6">
+            </div>
+            <div className=" border-b border-gray-300 py-2 px-6">
               <Link
                 href={"/india-trips"}
                 shallow
@@ -47,18 +47,18 @@ const Navbar = () => {
               >
                 Indian Packages
               </Link>
-            </li>
-            <li className=" border-b border-gray-300 py-2 px-6">
+            </div>
+            <div className=" border-b border-gray-300 py-2 px-6">
               <Link href={"/"} shallow onClick={() => setOpen(false)}>
                 Weekend Trips
               </Link>
-            </li>
-            <li className=" border-b border-gray-300 py-2 px-6">
+            </div>
+            <div className=" border-b border-gray-300 py-2 px-6">
               <Link href={"/"} shallow onClick={() => setOpen(false)}>
                 {" "}
                 Group Tours
               </Link>
-            </li>
+            </div>
           </ul>
           <ul className="flex flex-col font-medium text-white justify-center items-stretch px-6 py-3 gap-3">
             <li>Blogs</li>
@@ -91,7 +91,7 @@ const Navbar = () => {
       </div>
       <div className="p-2 z-20 flex justify-between items-center fixed w-full bg-darkvl sm:hidden">
         <ul className="flex gap-10 ml-10 text-white">
-          <li className="group/item relative">
+          <div className="group/item relative">
             <Link
               href={"/international-trips"}
               shallow
@@ -125,8 +125,8 @@ const Navbar = () => {
                 </div>
               </ul>
             </div>
-          </li>
-          <li className="group/item relative">
+          </div>
+          <div className="group/item relative">
             <Link
               href={"/india-trips"}
               shallow
@@ -175,8 +175,8 @@ const Navbar = () => {
                 </div>
               </ul>
             </div>
-          </li>
-          <li className="group/item relative">
+          </div>
+          <div className="group/item relative">
             <div className="flex justify-center items-center my-2 gap-1 cursor-pointer">
               Weekend Trips <IoIosArrowDown />
             </div>
@@ -206,8 +206,8 @@ const Navbar = () => {
                 </div>
               </ul>
             </div>
-          </li>
-          <li className="group/item relative">
+          </div>
+          <div className="group/item relative">
             <div className="flex justify-center items-center my-2 gap-1 cursor-pointer">
               Group Tours <IoIosArrowDown />
             </div>
@@ -237,7 +237,7 @@ const Navbar = () => {
                 </div>
               </ul>
             </div>
-          </li>
+          </div>
         </ul>
       </div>
     </nav>
